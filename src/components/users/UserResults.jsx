3d8496116db08be6222e285c0d338902023bf46a
PareshHK <phatikankan@gmail.com>
2024-01-27 14:39:20 +0530

    spinner

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-
+import Spinner from '../layout/Spinner'
 
 function UserResults() {
     const [users, setUsers] = useState([])
@@ -22,19 +22,25 @@ function UserResults() {
             })
             
             const data = await response.json()
-            console.log(data);
+            setUsers(data);
             setLoading(false);
 
     }
       
+    if (!loading ){
+        return (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+                {users.map((user) => (
+                <h3>{user.login}</h3>
+                ))}
+            </div>
+         )
 
-  return (
-    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg: grid-cols-3 md:grid-cols-2'>
-        {users.map((user) => 
-        <h3>{user.login}</h3>
-        )}
-    </div>
-  )
+    } 
+    else {
+        return <Spinner />
+    }
+  
 }
 
 export default UserResults
