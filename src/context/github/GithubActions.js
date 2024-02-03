c457e9b1e9a1f2eb4335142dcb6217bc7473ba6b
PareshHK <phatikankan@gmail.com>
2024-02-03 14:55:45 +0530

    adding users and user in githubaction

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,6 +1,11 @@
+
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
+
+
+// search users
 export const searchUsers = async (text) => {
     
 
@@ -22,3 +27,55 @@ export const searchUsers = async (text) => {
         return items
         }
         
+        // get users
+        export const getUser = async (login) => {
+            
+    
+            
+    
+            const response = await fetch (
+                `${GITHUB_URL}/users/${login}`,{
+                    headers : {
+                        Authorization: `token ${ GITHUB_TOKEN}`,
+    
+                        
+                    }            
+                })
+                
+                if(response.status === 404){
+                    window.location = '/notfound'
+                } else{
+                    const data = await response.json()
+                
+                return data
+                }
+
+                }
+
+                
+            
+            // Get user repos
+            export const getUserRepos = async (login) => {
+                
+
+                const params = new URLSearchParams({
+                    sort: 'created',
+                    per_page: 10
+                })
+        
+                
+        
+                const response = await fetch (
+                    `${GITHUB_URL}/users/${login}/repos?${params}`,{
+                        headers : {
+                            Authorization: `token ${ GITHUB_TOKEN}`,
+        
+                            
+                        }            
+                    })
+                    
+                    const data = await response.json()
+                    
+                    return data
+                    }
+            
